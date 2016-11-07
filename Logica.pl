029cf7fbc38de9ccc07274cb9dc4bf8c8bcea0d3
FranciscoBarbosa <franciscolamares@gmail.com>
2016-11-07 15:05:32 +0000

    logica changed

--- Logica.pl
+++ Logica.pl
@@ -1,8 +1,5 @@
 :-include('tabuleiro.pl').
-
-
-checkPos(List,X,Y):-getPiece(X,Y,vazio,List).%Piece\==vazio.		
-
+:-include('lib(iso_light)').
 		
 
 play(List,X,Y,Piece,NewList):-checkPos(List,X,Y),setPiece(List,X,Y,Piece,NewList),printBoard(NewList).
@@ -14,11 +11,14 @@ play(List,X,Y,Piece,NewList):-printBoard(List),write('Nao e possivel jogar nessa
 checkAround(List,X,Y,NewList):-X1 is X-1, X2 is X+1, Y1 is Y-1,Y2 is Y2+1,getPiece(X1,Y),getPiece(X2,Y),getPiece(X,Y1),getPiece(X,Y2).
 
 
-sum([],Result).
-sum([H|T],Result):-Result1 is Result+H,Sum(T,Result1).
+sum([],Result,Var):-Var=Result.
+sum([H|T],Result,Var):-Result1 is Result+H,sum(T,Result1,Var).
 
 deletePieces().%escolher quais comer
 
-%fazer funcao para para peça w(Result).
+%fazer funcao para peça w(Result).
+
+newPiece(List,Piece):-sum(List,Result),number_chars(Result,R),atom_chars('w',W),append(W,R,FinalPiece),atom_chars(FinalPiece,Final),Piece=Final.
+
 
 eatPieces(List,NewPiece,X,Y,NewList):-sum(List,Result),Result <6,deletePieces(),setPiece().
